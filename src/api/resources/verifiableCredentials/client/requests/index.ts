c2a2ce781bb4a624ca239e16739fdd190e1235dc
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-24 16:32:42 +0000

    SDK regeneration

---
+++ src/api/resources/verifiableCredentials/client/requests/index.ts
@@ -0,0 +1,5 @@
+export { type VerifyCredentialRequest } from "./VerifyCredentialRequest";
+export { type VcCreateTemplateRequest } from "./VcCreateTemplateRequest";
+export { type IssueVcRequest } from "./IssueVcRequest";
+export { type CreateTypeRequest } from "./CreateTypeRequest";
+export { type CreateTypeIdempotentRequest } from "./CreateTypeIdempotentRequest";
